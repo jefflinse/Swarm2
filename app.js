024302bf37d83926272db13dc1a7c741ef9622e3
Jeff Linse <jeff@jefflinse.com>
2017-10-23 15:19:18 -0700

    Simplify app.js

--- app.js
+++ app.js
@@ -2,14 +2,8 @@
 
 var World = require('./world');
 
-function start(numCreatures)
-{
-	var canvas = document.getElementById('canvas');
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+var canvas = document.getElementById('canvas');
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-	var world = new World(numCreatures, canvas);
-	world.start();
-};
-
-start(100);
+new World(100, canvas).start();
